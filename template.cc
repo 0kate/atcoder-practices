8c0c1e4fac2a90b1fe3299d6e1ebee1ec965f356
0Kate <o.keito317@gmail.com>
2020-05-10 17:08:46 +0900

    update

--- template.cc
+++ template.cc
@@ -6,6 +6,17 @@ typedef long long ll;
  
 using namespace std;
 
+template <typename T> T gcd(T a, T b) {
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+template <typename T> T lcm(T a, T b) {
+    return (a * b) / gcd(a, b);
+}
+
 void solve() {
 }
 
