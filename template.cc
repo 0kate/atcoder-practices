4058e1acb8591824e61b01739ac125795edc3941
0Kate <o.keito317@gmail.com>
2020-05-17 23:00:39 +0900

    update

--- template.cc
+++ template.cc
@@ -6,15 +6,28 @@ typedef long long ll;
  
 using namespace std;
 
-template <typename T> T gcd(T a, T b) {
+template <typename T>
+void precision_cout(T x, int n, bool is_endl = false) {
+    cout << fixed << setprecision(n) << x;
+    if (is_endl) cout << endl;
+}
+
+template <typename T>
+T gcd(T a, T b) {
     if (b == 0)
         return a;
 
     return gcd(b, a % b);
 }
 
-template <typename T> T lcm(T a, T b) {
+template <typename T>
+T lcm(T a, T b) {
     return (a * b) / gcd(a, b);
+}
+
+template <typename T>
+T radian(T angle) {
+    return angle * M_PI / 180.0;
 }
 
 void solve() {
